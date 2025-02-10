81facf8aa87e7c68b53e33cec061ef1e5b7b133c
JesusD2405 <jesusdavid2405@gmail.com>
2025-02-10 07:37:02 -0400

    feature: adding base repository for total order calculation

--- src/domain/usecases/order/getOrderUsecases.py
+++ src/domain/usecases/order/getOrderUsecases.py
@@ -1,8 +1,12 @@
 
+from .calculateTotalOrderUsecases import CalculateTotalOrderUsecases
 class GetOrderUsecases(object):
 
     def __init__(self, order_repo):
         self._order_repo = order_repo
 
     def execute(self):
-        return self._order_repo.get()
+        self._order_repo.get()
+        total_order = CalculateTotalOrderUsecases(self._order_repo).execute()
+        
+        return total_order
